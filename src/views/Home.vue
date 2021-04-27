3f97889f0bf39007289c0c9308064623556aa8c1
TheCapn-MEC <erolda266@west-mec.org>
2021-04-26 17:43:19 -0700

    Started landing page
    Setup up cards for each of the pokemon, will need to polish up eventually

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,82 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Welcome to the Charmander Evolution Line Pokedex!</h1>
+    <div id="pokemon">
+      <div class="pokemonEntry">
+        <table>
+          <tr>
+            <td><h2>Charmander</h2></td>
+          </tr>
+          <tr>
+            <td><img src="@/assets/charmander.jpg" /></td>
+          </tr>
+          <tr>
+            <td><button>Go to entry</button></td>
+          </tr>
+        </table>
+      </div>
+      <div class="pokemonEntry">
+        <table>
+          <tr>
+            <td><h2>Charmeleon</h2></td>
+          </tr>
+          <tr>
+            <td><img src="@/assets/charmeleon.jpg" /></td>
+          </tr>
+          <tr>
+            <td><button>Go to entry</button></td>
+          </tr>
+        </table>
+      </div>
+      <div class="pokemonEntry">
+        <table>
+          <tr>
+            <td><h2>Charizrd</h2></td>
+          </tr>
+          <tr>
+            <td><img src="@/assets/charizard.jpg" /></td>
+          </tr>
+          <tr>
+            <td><button>Go to entry</button></td>
+          </tr>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style lang="scss" scoped>
+body {
+  margin: 0;
+  padding: 0;
+  background-color: red;
+}
 
+#pokemon {
+  display: flex;
+  text-align: center;
+  justify-content: space-evenly;
+  width: 100%;
+  div.pokemonEntry {
+    width: 20%;
+    border: 0.1rem black solid;
+    border-radius: 1rem;
+    background-color: white;
+    display: flex;
+    align-content: flex-start;
+    table {
+      width: 100%;
+    }
+    img {
+      width: 100%;
+    }
+  }
+}
+</style>
+
+<script>
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
