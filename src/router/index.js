f25463b3a1634c38abf6b6ac00629527f307c1e7
Giles-Aiden <agiles257@west-mec.org>
2021-04-26 17:41:37 -0700

    Pokemon Components

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,25 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/charmander",
+    name: "Charmander",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "charmander" */ "../views/Charmander.vue"),
+  },
+  {
+    path: "/charmeleon",
+    name: "Charmeleon",
+    component: () =>
+      import(/* webpackChunkName: "charmander" */ "../views/Charmander.vue"),
+  },
+  {
+    path: "/charizrd",
+    name: "Charizrd",
+    component: () =>
+      import(/* webpackChunkName: "charmander" */ "../views/Charmander.vue"),
   },
 ];
 
---
+++ src/views/Charmander.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <Pokemon />
+  </div>
+</template>
+
+<script>
+import Pokemon from "@/components/Pokemon.vue";
+export default {
+  name: "Home",
+  components: {
+    Pokemon,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
